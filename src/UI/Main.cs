JarrodDoyle/KCModelEditor

commit 52b04f37b2cc6147cd7b85cb62324215ab2d3950

fix: stale state after changing to a different install

src/UI/Main.cs
@@ -1,6 +1,5 @@
 using System;
 using Godot;
-using KeepersCompound.Dark;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
 
@@ -8,60 +7,18 @@ namespace KeepersCompound.ModelEditor.UI;
 
 public partial class Main : Node
 {
-    #region Nodes
-
-    private InstallManager _installManager = InstantiatePackedScene<InstallManager>("uid://dm8et7nwwnq34");
-    private ModelEditor _modelEditor = InstantiatePackedScene<ModelEditor>("uid://bmvch3t460c6k");
-
-    #endregion
-
-    #region Godot Overrides
-
-    public override void _EnterTree()
-    {
-        ConfigureLogger();
-    }
+    private const string InstallManagerSceneUid = "uid://dm8et7nwwnq34";
 
     public override void _Ready()
     {
-        _installManager.LoadInstall += InstallManagerOnLoadEditor;
-        _modelEditor.QuitToInstalls += ModelEditorOnQuitToInstalls;
-
-        AddChild(_installManager);
-    }
-
-    public override void _ExitTree()
-    {
-        _installManager.LoadInstall -= InstallManagerOnLoadEditor;
-        _modelEditor.QuitToInstalls -= ModelEditorOnQuitToInstalls;
-        _installManager.Config.Save();
-
-        _installManager.QueueFree();
-        _modelEditor.QueueFree();
-    }
-
-    #endregion
-
-    #region Event Handling
-
-    private void InstallManagerOnLoadEditor(string installPath)
-    {
-        var installContext = new InstallContext(installPath);
-        if (installContext.Valid)
+        ConfigureLogger();
+        var result = GetTree().ChangeSceneToFile(InstallManagerSceneUid);
+        if (result != Error.Ok)
         {
-            _modelEditor.SetEditorState(new EditorState(_installManager.Config, installContext));
-            RemoveChild(_installManager);
-            AddChild(_modelEditor);
+            Log.Error("Failed to change scene: {UID}", InstallManagerSceneUid);
+            GetTree().Quit();
         }
     }
-
-    private void ModelEditorOnQuitToInstalls()
-    {
-        RemoveChild(_modelEditor);
-        AddChild(_installManager);
-    }
-
-    #endregion
 
     private static void ConfigureLogger()
     {
@@ -75,10 +32,5 @@ public partial class Main : Node
         config.WriteTo.Console(theme: AnsiConsoleTheme.Sixteen, outputTemplate: outputTemplate);
         config.WriteTo.File(logPath, outputTemplate: outputTemplate);
         Log.Logger = config.CreateLogger();
-    }
-
-    private static T InstantiatePackedScene<T>(string uid) where T : Node
-    {
-        return (T)GD.Load<PackedScene>(uid).Instantiate();
     }
 }

src/UI/InstallManager.cs
@@ -2,18 +2,13 @@ using System.Collections.Generic;
 using System.Linq;
 using Godot;
 using KeepersCompound.Dark;
+using Serilog;
 
 namespace KeepersCompound.ModelEditor.UI;
 
 public partial class InstallManager : Control
 {
-    #region Events
-
-    public delegate void LoadInstallEventHandler(string installPath);
-
-    public event LoadInstallEventHandler? LoadInstall;
-
-    #endregion
+    private const string ModelEditorSceneUid = "uid://bmvch3t460c6k";
 
     #region Nodes
 
@@ -138,7 +133,21 @@ public partial class InstallManager : Control
         var path = _installPaths.GetItemText(idx);
         if (_validityMap.GetValueOrDefault(path, false))
         {
-            LoadInstall?.Invoke(path);
+            var context = new InstallContext(path);
+            if (!context.Valid)
+            {
+                Log.Error("Invalid install context, cannot load editor.");
+                return;
+            }
+
+            var editorState = new EditorState(Config, context);
+            var editor = (ModelEditor)GD.Load<PackedScene>(ModelEditorSceneUid).Instantiate();
+            editor.SetEditorState(editorState);
+            var result = GetTree().ChangeSceneToNode(editor);
+            if (result != Error.Ok)
+            {
+                Log.Error("Failed to change to editor scene.");
+            }
         }
     }
 

src/UI/ModelEditor.cs
@@ -2,11 +2,14 @@ using System.IO;
 using Godot;
 using KeepersCompound.ModelEditor.Render;
 using KeepersCompound.ModelEditor.UI.Menu;
+using Serilog;
 
 namespace KeepersCompound.ModelEditor.UI;
 
 public partial class ModelEditor : Control
 {
+    private const string InstallManagerSceneUid = "uid://dm8et7nwwnq34";
+
     private EditorState _state = null!;
     private ModelDocument? _document;
 
@@ -17,14 +20,6 @@ public partial class ModelEditor : Control
     private ModelViewport _modelViewport = null!;
     private ModelInspector _modelInspector = null!;
     private FileDialog _saveAsDialog = null!;
-
-    #endregion
-
-    #region Events
-
-    public delegate void QuitToInstallsEventHandler();
-
-    public event QuitToInstallsEventHandler? QuitToInstalls;
 
     #endregion
 
@@ -113,7 +108,12 @@ public partial class ModelEditor : Control
     private void EditorMenuOnQuitToInstallsPressed()
     {
         // TODO: Handle saving dirty file
-        QuitToInstalls?.Invoke();
+        var result = GetTree().ChangeSceneToFile(InstallManagerSceneUid);
+        if (result != Error.Ok)
+        {
+            Log.Error("Failed to change scene: {UID}", InstallManagerSceneUid);
+            GetTree().Quit();
+        }
     }
 
     private void EditorMenuOnRefocusCameraPressed()

commit e34e71cacc43af0cd58bdd5836d3cc26a6e32c3d

fix: godot scene change error
Introduced in 52b04f37 but easy enough to fix here

src/UI/Main.cs
@@ -15,6 +15,12 @@ public partial class Main : Node
     public void OnResolved()
     {
         ConfigureLogger();
+        SetProcess(true);
+    }
+
+    public void OnProcess(double delta)
+    {
+        // We do this in OnProcess to avoid the issues described here: https://github.com/godotengine/godot/issues/99651#issuecomment-2807113330
         var result = GetTree().ChangeSceneToFile(SceneUids.InstallManager);
         if (result != Error.Ok)
         {